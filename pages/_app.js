6601547b7a9eae23774cb9c7efe29424c6cc8fb7
Duong Bon Jones <iamtdthewinner@gmail.com>
2021-08-11 19:36:26 +0700

    fix service-worker

--- pages/_app.js
+++ pages/_app.js
@@ -4,5 +4,8 @@ import React from "react";
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.register();
 export default MyApp;
